972dc8ae98f5db5726b66420fc8bb90a2301b36d
noopys <loganschottland@gmail.com>
2023-05-18 19:36:32 -0400

    Sandy picture in

--- src/ui-components/FeatureListLeft.jsx
+++ src/ui-components/FeatureListLeft.jsx
@@ -7,12 +7,15 @@
 /* eslint-disable */
 import * as React from "react";
 import { getOverrideProps } from "@aws-amplify/ui-react/internal";
-import { Icon, Text, View } from "@aws-amplify/ui-react";
-export default function FeatureListLeft(props) {
+import { Text, View } from "@aws-amplify/ui-react";
+import ButtonSecondary from "./ButtonSecondary";
+import vab from '../resources/loganvab.jpg';
+export default function FeatureSplitLeft(props) {
   const { overrides, ...rest } = props;
   return (
     <View
-      width="1440px"
+      overflowX="hidden"
+      width="110%"
       height="600px"
       display="block"
       gap="unset"
@@ -20,7 +23,7 @@ export default function FeatureListLeft(props) {
       justifyContent="unset"
       position="relative"
       padding="0px 0px 0px 0px"
-      {...getOverrideProps(overrides, "FeatureListLeft")}
+      {...getOverrideProps(overrides, "FeatureSplitLeft")}
       {...rest}
     >
       <View
@@ -40,41 +43,101 @@ export default function FeatureListLeft(props) {
         {...getOverrideProps(overrides, "Background")}
       ></View>
       <View
-        width="480px"
-        height="360px"
+        padding="0px 0px 0px 0px"
+        width="720px"
+        height="600px"
         display="block"
         gap="unset"
         alignItems="unset"
         justifyContent="unset"
         position="absolute"
-        top="20%"
-        bottom="20%"
-        left="53.4%"
-        right="13.26%"
-        border="1px SOLID rgba(0,0,0,1)"
-        borderRadius="8px"
-        padding="0px 0px 0px 0px"
-        backgroundColor="rgba(255,255,255,1)"
+        top="0%"
+        bottom="0%"
+        left="50%"
+        right="0%"
         {...getOverrideProps(overrides, "Media")}
-      ></View>
+      >
+        <View
+          width="720px"
+          height="600px"
+          display="block"
+          gap="unset"
+          alignItems="unset"
+          justifyContent="unset"
+          position="absolute"
+          top="0%"
+          bottom="0%"
+          left="0%"
+          right="0%"
+          padding="0px 0px 0px 0px"
+          backgroundColor="rgba(105,105,105,1)"
+          {...getOverrideProps(overrides, "Media Background")}
+        ></View>
+        <View
+          width="320px"
+          height="360px"
+          display="block"
+          gap="unset"
+          alignItems="unset"
+          justifyContent="unset"
+          position="absolute"
+          top="20%"
+          bottom="20%"
+          left="27.5%"
+          right="28.06%"
+          borderRadius="8px"
+          padding="0px 0px 0px 0px"
+          backgroundColor="rgba(105,105,105,1)"
+          {...getOverrideProps(overrides, "Media Object")}
+        >
+          <img
+            src={vab}
+            alt="Brain Buffs Logo"
+            style={{
+              borderRadius:"10%",
+              width: '320px',
+              height: '500px',
+              position: 'absolute',
+              top: '50%',
+              left: '0', // Adjust the value based on your layout
+              transform: 'translateY(-50%)',
+            }}
+          />
+        </View>
+      </View>
       <View
         padding="0px 0px 0px 0px"
-        width="457px"
-        height="315px"
+        width="422px"
+        height="243px"
         display="block"
         gap="unset"
         alignItems="unset"
         justifyContent="unset"
         position="absolute"
-        top="23.83%"
-        bottom="23.67%"
-        left="13.89%"
-        right="54.37%"
+        top="29.83%"
+        bottom="29.67%"
+        left="9.38%"
+        right="61.32%"
         {...getOverrideProps(overrides, "Content")}
       >
+        {/* <ButtonSecondary
+          width="200px"
+          height="60px"
+          display="block"
+          gap="unset"
+          alignItems="unset"
+          justifyContent="unset"
+          position="absolute"
+          top="75.31%"
+          bottom="0%"
+          left="0%"
+          right="52.61%"
+          padding="0px 0px 0px 0px"
+          {...getOverrideProps(overrides, "Button Secondary")}
+        ></ButtonSecondary> */}
         <Text
           fontFamily="Roboto"
-          fontSize="20px"
+          fontSize="16px"
           fontWeight="400"
           color="rgba(0,0,0,1)"
           lineHeight="28.125px"
@@ -87,21 +150,26 @@ export default function FeatureListLeft(props) {
           gap="unset"
           alignItems="unset"
           position="absolute"
-          top="16.83%"
-          bottom="56.19%"
-          left="7.66%"
+          top="20%"
+          bottom="34.57%"
+          left="0%"
           right="0%"
           padding="0px 0px 0px 0px"
           whiteSpace="pre-wrap"
-          children="and not monkeys so please buy our product i promise itll be worth your money"
+          children="Logan is a motivated computer science student with a background as a
+           former NASA intern, who excelled on the SAT and is now dedicated to helping others
+            achieve the same success. Logan understands firsthand the strategies and techniques needed to excel in each section. Combined with his 
+            strong problem-solving, logical reasoning, and analytical skills, he brings a unique perspective to 
+            his tutoring approach. Logan provides students with the tools and 
+            guidance they need to achieve their own exceptional SAT scores. With his passion, expertise, Logan is committed to empowering students to reach their full potential on the SAT."
           {...getOverrideProps(overrides, "Subtitle")}
         ></Text>
         <Text
           fontFamily="Roboto"
-          fontSize="20px"
+          fontSize="24px"
           fontWeight="700"
           color="rgba(0,0,0,1)"
-          lineHeight="28.125px"
+          lineHeight="33.75px"
           textAlign="left"
           display="block"
           direction="column"
@@ -111,217 +179,15 @@ export default function FeatureListLeft(props) {
           gap="unset"
           alignItems="unset"
           position="absolute"
-          top="0%"
-          bottom="90.79%"
-          left="7%"
-          right="46.61%"
+          top="0"
+          bottom="90.01%"
+          left="0%"
+          right="39.81%"
           padding="0px 0px 0px 0px"
           whiteSpace="pre-wrap"
-          children="We are smart"
+          children="Meet Brain Buffs"
           {...getOverrideProps(overrides, "Title")}
         ></Text>
-        <View
-          padding="0px 0px 0px 0px"
-          width="193px"
-          height="135px"
-          display="block"
-          gap="unset"
-          alignItems="unset"
-          justifyContent="unset"
-          position="absolute"
-          top="57.14%"
-          bottom="0%"
-          left="0%"
-          right="57.77%"
-          {...getOverrideProps(overrides, "List")}
-        >
-          <View
-            padding="0px 0px 0px 0px"
-            width="193px"
-            height="29px"
-            display="block"
-            gap="unset"
-            alignItems="unset"
-            justifyContent="unset"
-            position="absolute"
-            top="0%"
-            bottom="78.52%"
-            left="0%"
-            right="0%"
-            {...getOverrideProps(overrides, "List Item0235")}
-          >
-            <Text
-              fontFamily="Roboto"
-              fontSize="20px"
-              fontWeight="400"
-              color="rgba(0,0,0,1)"
-              lineHeight="28.125px"
-              textAlign="left"
-              display="block"
-              direction="column"
-              justifyContent="unset"
-              width="unset"
-              height="unset"
-              gap="unset"
-              alignItems="unset"
-              position="absolute"
-              top="0%"
-              bottom="0%"
-              left="18.13%"
-              right="0%"
-              padding="0px 0px 0px 0px"
-              whiteSpace="pre-wrap"
-              children="Benefit of Feature"
-              {...getOverrideProps(overrides, "List Item Text0234")}
-            ></Text>
-            <Icon
-              width="10px"
-              height="10px"
-              viewBox={{ minX: 0, minY: 0, width: 10, height: 10 }}
-              paths={[
-                {
-                  d: "M10 5C10 7.76142 7.76142 10 5 10C2.23858 10 0 7.76142 0 5C0 2.23858 2.23858 0 5 0C7.76142 0 10 2.23858 10 5Z",
-                  fill: "rgba(0,0,0,1)",
-                  fillRule: "nonzero",
-                },
-              ]}
-              display="block"
-              gap="unset"
-              alignItems="unset"
-              justifyContent="unset"
-              position="absolute"
-              top="27.59%"
-              bottom="37.93%"
-              left="0%"
-              right="94.82%"
-              {...getOverrideProps(overrides, "Ellipse0236")}
-            ></Icon>
-          </View>
-          <View
-            padding="0px 0px 0px 0px"
-            width="193px"
-            height="29px"
-            display="block"
-            gap="unset"
-            alignItems="unset"
-            justifyContent="unset"
-            position="absolute"
-            top="39.26%"
-            bottom="39.26%"
-            left="0%"
-            right="0%"
-            {...getOverrideProps(overrides, "List Item0238")}
-          >
-            <Text
-              fontFamily="Roboto"
-              fontSize="20px"
-              fontWeight="400"
-              color="rgba(0,0,0,1)"
-              lineHeight="28.125px"
-              textAlign="left"
-              display="block"
-              direction="column"
-              justifyContent="unset"
-              width="unset"
-              height="unset"
-              gap="unset"
-              alignItems="unset"
-              position="absolute"
-              top="0%"
-              bottom="0%"
-              left="18.13%"
-              right="0%"
-              padding="0px 0px 0px 0px"
-              whiteSpace="pre-wrap"
-              children="Benefit of Feature"
-              {...getOverrideProps(overrides, "List Item Text0239")}
-            ></Text>
-            <Icon
-              width="10px"
-              height="10px"
-              viewBox={{ minX: 0, minY: 0, width: 10, height: 10 }}
-              paths={[
-                {
-                  d: "M10 5C10 7.76142 7.76142 10 5 10C2.23858 10 0 7.76142 0 5C0 2.23858 2.23858 0 5 0C7.76142 0 10 2.23858 10 5Z",
-                  fill: "rgba(0,0,0,1)",
-                  fillRule: "nonzero",
-                },
-              ]}
-              display="block"
-              gap="unset"
-              alignItems="unset"
-              justifyContent="unset"
-              position="absolute"
-              top="27.59%"
-              bottom="37.93%"
-              left="0%"
-              right="94.82%"
-              {...getOverrideProps(overrides, "Ellipse0240")}
-            ></Icon>
-          </View>
-          <View
-            padding="0px 0px 0px 0px"
-            width="193px"
-            height="29px"
-            display="block"
-            gap="unset"
-            alignItems="unset"
-            justifyContent="unset"
-            position="absolute"
-            top="78.52%"
-            bottom="0%"
-            left="0%"
-            right="0%"
-            {...getOverrideProps(overrides, "List Item0241")}
-          >
-            <Text
-              fontFamily="Roboto"
-              fontSize="20px"
-              fontWeight="400"
-              color="rgba(0,0,0,1)"
-              lineHeight="28.125px"
-              textAlign="left"
-              display="block"
-              direction="column"
-              justifyContent="unset"
-              width="unset"
-              height="unset"
-              gap="unset"
-              alignItems="unset"
-              position="absolute"
-              top="0%"
-              bottom="0%"
-              left="18.13%"
-              right="0%"
-              padding="0px 0px 0px 0px"
-              whiteSpace="pre-wrap"
-              children="Benefit of Feature"
-              {...getOverrideProps(overrides, "List Item Text0242")}
-            ></Text>
-            <Icon
-              width="10px"
-              height="10px"
-              viewBox={{ minX: 0, minY: 0, width: 10, height: 10 }}
-              paths={[
-                {
-                  d: "M10 5C10 7.76142 7.76142 10 5 10C2.23858 10 0 7.76142 0 5C0 2.23858 2.23858 0 5 0C7.76142 0 10 2.23858 10 5Z",
-                  fill: "rgba(0,0,0,1)",
-                  fillRule: "nonzero",
-                },
-              ]}
-              display="block"
-              gap="unset"
-              alignItems="unset"
-              justifyContent="unset"
-              position="absolute"
-              top="27.59%"
-              bottom="37.93%"
-              left="0%"
-              right="94.82%"
-              {...getOverrideProps(overrides, "Ellipse0243")}
-            ></Icon>
-          </View>
-        </View>
       </View>
     </View>
   );
--- src/ui-components/FeatureSplitRight.jsx
+++ src/ui-components/FeatureSplitRight.jsx
@@ -1,23 +1,19 @@
-/***************************************************************************
- * The contents of this file were generated with Amplify Studio.           *
- * Please refrain from making any modifications to this file.              *
- * Any changes to this file will be overwritten when running amplify pull. *
- **************************************************************************/
-
 /* eslint-disable */
-import * as React from "react";
+import React from "react";
 import { getOverrideProps } from "@aws-amplify/ui-react/internal";
 import { Text, View } from "@aws-amplify/ui-react";
 import ButtonSecondary from "./ButtonSecondary";
-import vab from '../resources/loganvab.jpg';
+import vab from '../resources/sandy_headshot.jpg';
+
 export default function FeatureSplitRight(props) {
   const { overrides, ...rest } = props;
+
   return (
     <View
       overflowX="hidden"
       width="110%"
       height="600px"
-      display="block"
+      display="flex"
       gap="unset"
       alignItems="unset"
       justifyContent="unset"
@@ -46,64 +42,37 @@ export default function FeatureSplitRight(props) {
         padding="0px 0px 0px 0px"
         width="720px"
         height="600px"
-        display="block"
+        display="flex"
         gap="unset"
-        alignItems="unset"
-        justifyContent="unset"
+        alignItems="center"
+        justifyContent="center"
         position="absolute"
         top="0%"
         bottom="0%"
-        left="50%"
+        left="55%"
         right="0%"
         {...getOverrideProps(overrides, "Media")}
       >
-        <View
-          width="720px"
-          height="600px"
-          display="block"
-          gap="unset"
-          alignItems="unset"
-          justifyContent="unset"
-          position="absolute"
-          top="0%"
-          bottom="0%"
-          left="0%"
-          right="0%"
-          padding="0px 0px 0px 0px"
-          backgroundColor="rgba(105,105,105,1)"
-          {...getOverrideProps(overrides, "Media Background")}
-        ></View>
-        <View
-          width="320px"
-          height="360px"
-          display="block"
-          gap="unset"
-          alignItems="unset"
-          justifyContent="unset"
-          position="absolute"
-          top="20%"
-          bottom="20%"
-          left="27.5%"
-          right="28.06%"
-          borderRadius="8px"
-          padding="0px 0px 0px 0px"
-          backgroundColor="rgba(105,105,105,1)"
-          {...getOverrideProps(overrides, "Media Object")}
+        <div
+          style={{
+            //width: '100%',
+            // maxWidth: '100%',
+            height: '600px',
+            //maxHeight: '100%',
+            // borderRadius: '8px',
+            overflow: 'hidden',
+          }}
         >
           <img
             src={vab}
             alt="Brain Buffs Logo"
             style={{
-              borderRadius:"10%",
-              width: '360px',
-              height: '500px',
-              position: 'absolute',
-              top: '50%',
-              left: '0', // Adjust the value based on your layout
-              transform: 'translateY(-50%)',
+              paddingLeft:'20px',
+              width: '100%',
+              height: '100%',
             }}
           />
-        </View>
+        </div>
       </View>
       <View
         padding="0px 0px 0px 0px"
@@ -114,51 +83,12 @@ export default function FeatureSplitRight(props) {
         alignItems="unset"
         justifyContent="unset"
         position="absolute"
-        top="29.83%"
+        top="0%"
         bottom="29.67%"
         left="9.38%"
         right="61.32%"
         {...getOverrideProps(overrides, "Content")}
       >
-        {/* <ButtonSecondary
-          width="200px"
-          height="60px"
-          display="block"
-          gap="unset"
-          alignItems="unset"
-          justifyContent="unset"
-          position="absolute"
-          top="75.31%"
-          bottom="0%"
-          left="0%"
-          right="52.61%"
-          padding="0px 0px 0px 0px"
-          {...getOverrideProps(overrides, "Button Secondary")}
-        ></ButtonSecondary> */}
-        <Text
-          fontFamily="Roboto"
-          fontSize="16px"
-          fontWeight="400"
-          color="rgba(0,0,0,1)"
-          lineHeight="28.125px"
-          textAlign="left"
-          display="block"
-          direction="column"
-          justifyContent="unset"
-          width="422px"
-          height="unset"
-          gap="unset"
-          alignItems="unset"
-          position="absolute"
-          top="20%"
-          bottom="34.57%"
-          left="0%"
-          right="0%"
-          padding="0px 0px 0px 0px"
-          whiteSpace="pre-wrap"
-          children="Sandy is a monkey"
-          {...getOverrideProps(overrides, "Subtitle")}
-        ></Text>
         <Text
           fontFamily="Roboto"
           fontSize="24px"
@@ -183,6 +113,31 @@ export default function FeatureSplitRight(props) {
           children="Meet Brain Buffs"
           {...getOverrideProps(overrides, "Title")}
         ></Text>
+        <Text
+          fontFamily="Roboto"
+          fontSize="16px"
+          fontWeight="400"
+          color="rgba(0,0,0,0,1)"
+          lineHeight="28.125px"
+          textAlign="left"
+          display="block"
+          direction="column"
+          justifyContent="unset"
+          width="422px"
+          height="unset"
+          gap="unset"
+          alignItems="unset"
+          position="absolute"
+          top="20%"
+          bottom="34.57%"
+          left="0%"
+          right="0%"
+          padding="0px 0px 0px 0px"
+          whiteSpace="pre-wrap"
+          children="Sandy is a monkey"
+          {...getOverrideProps(overrides, "Subtitle")}
+        ></Text>
+        {/* Rest of the code... */}
       </View>
     </View>
   );
