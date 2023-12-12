1ece2b9b152fa264437f28d53410834b3ac74b4e
IsaacLi21 <144957190+IsaacLi4@users.noreply.github.com>
2023-12-12 13:20:18 -0700

    Store session token locally
    Allows for page refresh with resumed session

--- src/components/AuthContext.jsx
+++ src/components/AuthContext.jsx
@@ -1,5 +1,5 @@
 // AuthContext.js
-import React, { useState, useContext} from 'react';
+import React, { useState, useContext, useEffect} from 'react';
 import { Auth } from 'aws-amplify';
 
 const AuthContext = React.createContext();
@@ -12,16 +12,39 @@ export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    loadSessionToken();
+  }, []);
+
+  const loadSessionToken = async () => {
+    try {
+      const sessionToken = localStorage.getItem('sessionToken');
+      if (sessionToken) {
+        // If session token is available, set the user and mark as logged in
+        setIsLoggedIn(true);
+        setUser(JSON.parse(sessionToken)); // Assuming sessionToken is a JSON string
+      }
+    } catch (error) {
+      console.error('Error loading session token:', error);
+    }
+  };
+
   const login = (userData) => {
-    // Perform authentication logic here
     // Set isLoggedIn to true and set user data upon successful login
     setIsLoggedIn(true);
     setUser(userData);
+
+    // Store the session token locally
+    localStorage.setItem('sessionToken', JSON.stringify(userData));
   };
 
   const logout = async () => {
     // Perform logout logic here
     await Auth.signOut();
+
+    // Clear the session token from localStorage
+    localStorage.removeItem('sessionToken');
+
     // Set isLoggedIn to false and clear user data
     setIsLoggedIn(false);
     setUser(null);
