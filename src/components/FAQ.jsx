fc31324c17350671ee5a42329c8d75eb7040c8a2
IsaacLi21 <144957190+IsaacLi4@users.noreply.github.com>
2023-12-15 13:13:34 -0700

    FAQ page update
    add annimations, colors, rephrase all FAQ and responses

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,24 +1,37 @@
-
-import { Disclosure } from '@headlessui/react'
+import { Disclosure, Transition } from '@headlessui/react'
 import { ChevronUpIcon } from '@heroicons/react/20/solid'
+import { useState } from 'react';
 
-export default function Example() {
+
+export default function FAQ() {
+
   return (
     <div className="w-full px-8 pt-16">
-      <div className="mx-auto w-full max-w-4xl rounded-2xl bg-white p-2">
+      <h1 style={{fontFamily: 'Arial, sans-serif', fontSize: '3em', fontWeight: 'bold'}}>Frequently Asked Questions</h1>
+      <div className="mx-auto w-full max-w-2xl rounded-2xl bg-white p-2">
         <Disclosure>
           {({ open }) => (
             <>
-              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-300 px-4 py-4 text-left text-sm font-medium text-gray-900 hover:bg-gray-200 focus:outline-none focus-visible:ring focus-visible:ring-gray-500/75">
-                <span>Can I do virtual sessions?</span>
+              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-300 px-4 py-4 text-left text-sm font-medium text-gray-900 hover:bg-gray-200 focus:outline-none focus-visible:ring focus-visible:ring-teal-500/75">
+                <span>Are virtual sessions available?</span>
                 <ChevronUpIcon
                   className={`${open ? 'rotate-180 transform' : ''
                     } h-5 w-5 text-black-500`}
                 />
               </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
-                Yes! We can do virtual sessions or in person sessions in the Boulder area.
+              <Transition
+                show={open}
+                enter="transition-all duration-400 ease-out"
+                enterFrom="opacity-0 translate-y-[-30%]"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition-all duration-200 ease-out"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-[-30%]"
+              >
+              <Disclosure.Panel className="px-4 pb-3 pt-3 text-sm text-black bg-green-100 text-left">
+                Yes! We proudly provide a versatile range of learning opportunities, offering both virtual and in-person sessions within the Boulder area.
               </Disclosure.Panel>
+              </Transition>
             </>
           )}
         </Disclosure>
@@ -26,15 +39,25 @@ export default function Example() {
           {({ open }) => (
             <>
               <Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-300 px-4 py-4 text-left text-sm font-medium text-graye-900 hover:bg-gray-200 focus:outline-none focus-visible:ring focus-visible:ring-gray-500/75">
-                <span>How do I know my scores will increase?</span>
+                <span>How do I know my SAT score will improve?</span>
                 <ChevronUpIcon
                   className={`${open ? 'rotate-180 transform' : ''
                     } h-5 w-5 text-black-500`}
                 />
               </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
-                We offer a money back guarantee. If your SAT score doesn't increase, you get 100% money back.
+              <Transition
+                show={open}
+                enter="transition-all duration-400 ease-out"
+                enterFrom="opacity-0 translate-y-[-30%]"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition-all duration-200 ease-out"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-[-30%]"
+              >
+              <Disclosure.Panel className="px-4 pb-3 pt-3 text-sm text-black bg-green-100 text-left">
+              We stand by our commitment with a robust money-back guarantee. If your SAT score doesn't demonstrate improvement, we offer a full refund.
               </Disclosure.Panel>
+              </Transition>
             </>
           )}
         </Disclosure>
@@ -48,9 +71,19 @@ export default function Example() {
                     } h-5 w-5 text-black-500`}
                 />
               </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
-                Of course we do! If you refer a friend and they join, both of you will receive a $50 discount off your tutoring package.
+              <Transition
+                show={open}
+                enter="transition-all duration-400 ease-out"
+                enterFrom="opacity-0 translate-y-[-30%]"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition-all duration-200 ease-out"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-[-30%]"
+              >
+              <Disclosure.Panel className="px-4 pb-3 pt-3 text-sm text-black bg-green-100 text-left">
+               We do indeed! When you refer a friend who joins our program, both of you will receive a $50 discount towards your tutoring package.
               </Disclosure.Panel>
+              </Transition>
             </>
           )}
         </Disclosure>
@@ -64,9 +97,19 @@ export default function Example() {
                     } h-5 w-5 text-black-500`}
                 />
               </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
-              Brain Buffs specializes in the new online SAT. Our tutors are all specifically trained with the new online SAT in mind, unlike tutors from other companies. We teach students how to succeed with the new test format in addition to the concepts they need to know
+              <Transition
+                show={open}
+                enter="transition-all duration-400 ease-out"
+                enterFrom="opacity-0 translate-y-[-30%]"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition-all duration-200 ease-out"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-[-30%]"
+              >
+              <Disclosure.Panel className="px-4 pb-3 pt-3 text-sm text-black bg-green-100 text-left">
+              At Brain Buffs, the new online SAT is an area of our expertise. Our tailored approach and specialized training prepare students to excel in this format, ensuring they are equipped with the necessary skills and strategies for success.
               </Disclosure.Panel>
+              </Transition>
             </>
           )}
         </Disclosure>
@@ -74,4 +117,3 @@ export default function Example() {
     </div>
   )
 }
-
