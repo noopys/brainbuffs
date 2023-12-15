1d570bcfb9330695d55111b3abd45e1f13a89736
IsaacLi21 <144957190+IsaacLi4@users.noreply.github.com>
2023-12-14 20:05:53 -0700

    updated sign up page

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -1,114 +1,3 @@
-// import React, { useState } from 'react';
-// import { Auth } from 'aws-amplify';
-// //import { withRouter  } from 'react-router-dom'; // Import useHistory hook
-// //import { withAuthenticator } from '@aws-amplify/ui-react';
-// import '@aws-amplify/ui-react/styles.css';
-// import { useAuth } from './AuthContext';
-
-
-// function SignIn() {
-//   //console.log('SignIn component rendered');
-//   const { isLoggedIn, user, login, logout } = useAuth();
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [errorMessage, setErrorMessage] = useState('');
-
-//   const buttonStyle = {
-//     backgroundColor: '#007bff', // Blue color
-//     color: '#fff', // White text
-//     padding: '8px 16px', // Adjust padding as needed
-//     border: 'none', // Remove border if needed
-//     borderRadius: '4px', // Add border-radius if needed
-//     cursor: 'pointer', // Show pointer on hover
-//   };
-
-//   const fieldStyle = {
-//     marginBottom: '10px',
-//     padding: '10px',
-//     border: '1px solid #ccc',
-//     borderRadius: '5px',
-//     fontSize: '16px',
-//   };
-
-//   const handleUsernameChange = (e) => {
-//     setUsername(e.target.value);
-//   };
-
-//   const handlePasswordChange = (e) => {
-//     setPassword(e.target.value);
-//   };
-
-//   const handleSignIn = async () => {
-//     try {
-//       // use Amplify API to sign in
-//       console.log("hello")
-//       const userInfo = await Auth.signIn(username, password);
-//       // set the email attribute
-
-//       login({email: userInfo.attributes.email});
-//     } catch (error) {
-//       console.error('Sign-in error:', error);
-//       setErrorMessage(error.message);
-//     }
-//   };
-
-//   const handleSignOut = async () => {
-//     try {
-//       logout();
-//     } catch (error) {
-//       console.error('Error signing out:', error);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       {isLoggedIn ? (
-//         <div>
-//           <h1>Welcome!</h1>
-//           <br></br>
-//           <h4>You are signed in with email: </h4>
-//           <h3>{user.email}</h3>
-//           <button onClick={handleSignOut} style={buttonStyle}>Sign Out</button>
-//         </div>
-//       ) : (
-//         <div>
-//           <h1>Welcome! Please Sign in with your account. </h1>
-//           <br></br>
-//           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center',  flexDirection: 'column'}}>
-//             <input
-//               type="text"
-//               placeholder="Email"
-//               value={username}
-//               onChange={handleUsernameChange}
-//               style={fieldStyle}
-//             />
-//             <input
-//               type="password"
-//               placeholder="Password"
-//               value={password}
-//               onChange={handlePasswordChange}
-//               style={fieldStyle}
-//             />
-//           </div>
-//           <button onClick={handleSignIn} style={buttonStyle}>Sign In</button>
-//           {errorMessage && <p>Error: {errorMessage}</p>}
-//           <br></br><br></br>
-//           <h5>Don't have an account?</h5>
-//           <a href="./signup"><button style={buttonStyle}> Create One</button></a>
-//           <br></br><br></br>
-//           <h5>Forgot your password?</h5>
-//           <a href="./resetPassword"><button style={buttonStyle}> Reset it</button></a>
-//           <br></br><br></br>
-//           <h6>Getting Error: User is not confirmed? Click below to verify your account</h6>
-//           <a href="./verificationCode"><button  style={buttonStyle}> Verify My Account</button></a>
-//         </div>
-//       )}
-//     </div>
-
-//   );
-// }
-
-// export default SignIn;
 import React, { useState } from 'react';
 import { Auth } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
@@ -124,7 +13,7 @@ function SignIn() {
     border: '1px solid #20a7a1',
     padding: '20px',
     borderRadius: '10px',
-    maxWidth: '400px',
+    width: '400px',
     margin: '50px auto',
     fontFamily: 'Arial, sans-serif',
     borderBottom: '1px solid #20a7a1',
@@ -139,6 +28,7 @@ function SignIn() {
     cursor: 'pointer',
     margin: '10px',
     boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+    width: '300px',
   };
 
   const inputStyle = {
@@ -153,7 +43,7 @@ function SignIn() {
   const lineStyle = {
     border: 'none',
     borderTop: '1px solid #20a7a1', // Match the border style
-    margin: '20px auto',
+    margin: '30px auto',
     width: '400px',
   };
 
@@ -186,7 +76,7 @@ function SignIn() {
   return (
     <div>
       <div style={containerStyle}>
-        <h1>{isLoggedIn ? 'You are signed in.' : 'Sign In'}</h1>
+        <h1 style={{fontFamily: 'Arial, sans-serif', fontSize: '2em', fontWeight: 'bold', textTransform: 'capitalize'}}>{isLoggedIn ? 'You are signed in.' : 'Sign In'}</h1>
         {!isLoggedIn ? (
           <div>
             <div>
@@ -211,8 +101,8 @@ function SignIn() {
               
               <div>
                 <p>Need Help?</p>
-                <p><a href="./resetPassword">Reset Password</a></p>
-                <p><a href="./verificationCode">Verify Your Account</a></p>
+                <p><a href="./resetPassword" style={{textDecoration: 'none'}}>Reset Password</a></p>
+                <p><a href="./verificationCode" style={{textDecoration: 'none'}}>Verify Your Account</a></p>
               </div>
             </div>
           </div>
@@ -226,7 +116,7 @@ function SignIn() {
       {isLoggedIn ? ("") : (
         <div>
           <div style={lineStyle}></div>
-          <p>New to Brain Buffs? <a href="./signUp">Create Account</a></p>
+          <p>New to Brain Buffs? <a href="./signUp" style={{textDecoration: 'none'}}>Create an Account</a></p>
         </div>
       )}
       
--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -2,34 +2,46 @@ import React, { useState, useEffect } from 'react';
 import { Auth } from 'aws-amplify';
 
 function SignUp() {
-  //sign up
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [passwordsMatch, setPasswordsMatch] = useState(false);
 
-  //verification
   const [verificationCode, setVerificationCode] = useState('');
   const [verificationError, setVerificationError] = useState('');
   const [ver, setVer] = useState(false);
   const [succ, setSucc] = useState(false);
 
-  const buttonStyle = {
-    backgroundColor: '#007bff', // Blue color
-    color: '#fff', // White text
-    padding: '8px 16px', // Adjust padding as needed
-    border: 'none', // Remove border if needed
-    borderRadius: '4px', // Add border-radius if needed
-    cursor: 'pointer', // Show pointer on hover
+  const containerStyle = {
+    border: '1px solid #20a7a1',
+    padding: '20px',
+    borderRadius: '10px',
+    width: '400px',
+    margin: '50px auto',
+    fontFamily: 'Arial, sans-serif',
+    borderBottom: '1px solid #20a7a1',
   };
-
-  const fieldStyle = {
-    marginBottom: '10px',
-    padding: '10px',
+  
+  const buttonStyle = {
+    backgroundColor: '#20a7a1',
+    color: '#fff',
+    padding: '12px 24px',
+    border: 'none',
+    borderRadius: '6px',
+    cursor: 'pointer',
+    margin: '10px',
+    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+    width: '300px',
+  };
+  
+  const inputStyle = {
+    padding: '12px',
     border: '1px solid #ccc',
-    borderRadius: '5px',
+    borderRadius: '6px',
     fontSize: '16px',
+    width: '300px',
+    marginBottom: '10px',
   };
 
   const handleUsernameChange = (e) => {
@@ -45,7 +57,6 @@ function SignUp() {
   };
 
   useEffect(() => {
-    // Check if passwords match whenever password or confirmPassword changes
     setPasswordsMatch(password === confirmPassword);
   }, [password, confirmPassword]);
 
@@ -56,15 +67,11 @@ function SignUp() {
         return;
       }
 
-      await Auth.signUp({username, password});
-      //await remember({email: username});
-      console.log("signing up user", username);
-      // Handle successful sign-up, such as showing a success message or redirecting the user
+      await Auth.signUp({ username, password });
       setVer(true);
     } catch (error) {
       console.error('Sign-up error:', error);
       setErrorMessage(error.message);
-      // Handle sign-up errors, display an error message, etc.
     }
   };
 
@@ -74,7 +81,7 @@ function SignUp() {
         setVerificationError('Verification code must be a 6-digit number');
         return;
       }
-      
+
       await Auth.confirmSignUp(username, verificationCode);
       setSucc(true);
     } catch (error) {
@@ -83,64 +90,73 @@ function SignUp() {
   };
 
   return (
-    <div>
-      {ver ? 
-      ( succ ? (
-        <div>
-          <h2>You have successfully made an account!</h2>
-          <h3>The next step is to sign in.</h3>
-          <a href="./signin"><button style={buttonStyle}> Sign in</button></a>
-        </div>
+    <div style={containerStyle}>
+      {ver ? (
+        succ ? (
+          <div>
+            <h2>You have successfully made an account!</h2>
+            <h3>The next step is to sign in.</h3>
+            <a href="./signin">
+              <button style={buttonStyle}>Sign in</button>
+            </a>
+          </div>
+        ) : (
+          <div>
+            <h2>Please Enter the Verification Code Sent to Your Email</h2>
+            <input
+              type="text"
+              placeholder="Verification Code"
+              value={verificationCode}
+              style={inputStyle}
+              onChange={(e) => setVerificationCode(e.target.value)}
+            />
+            <br />
+            <button onClick={handleVerification} style={buttonStyle}>
+              Verify
+            </button>
+            {verificationError && <p>{verificationError}</p>}
+          </div>
+        )
       ) : (
         <div>
-          <h2>Please Enter the Verification Code Sent to Your Email</h2>
-          <input
-            type="text"
-            placeholder="Verification Code"
-            value={verificationCode}
-            style={fieldStyle}
-            onChange={(e) => setVerificationCode(e.target.value)}
-          />
+          <h1 style={{fontFamily: 'Arial, sans-serif', fontSize: '2em', fontWeight: 'bold', textTransform: 'capitalize'}}>Sign Up</h1>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'column',
+            }}
+          >
+            <input
+              type="text"
+              placeholder="Email"
+              value={username}
+              onChange={handleUsernameChange}
+              style={inputStyle}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={handlePasswordChange}
+              style={inputStyle}
+            />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
+              style={inputStyle}
+            />
+          </div>
+          <button onClick={handleSignUp} style={buttonStyle}>Sign Up</button>
+          {errorMessage && <p>Error: {errorMessage}</p>}
           <br />
-          <button onClick={handleVerification} style={buttonStyle}>Verify</button>
-          {verificationError && <p>{verificationError}</p>}
+          <br />
+          <p>Already have an account? <a href="./signin" style={{textDecoration: 'none'}}>Sign In</a></p>
+          
         </div>
-      )
-
-      ) : (
-      <div>
-        <h1>Welcome to Brain Buffs Tutoring! </h1>
-        <h2>Please Sign Up Below!</h2>
-        <br />
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center',  flexDirection: 'column' }}>
-          <input
-            type="text"
-            placeholder="Email"
-            value={username}
-            onChange={handleUsernameChange}
-            style={fieldStyle}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={handlePasswordChange}
-            style={fieldStyle}
-          />
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            value={confirmPassword}
-            onChange={handleConfirmPasswordChange}
-            style={fieldStyle}
-          />
-        </div>
-        <button onClick={handleSignUp} style={buttonStyle}>Sign Up</button>
-        {errorMessage && <p>Error: {errorMessage}</p>}
-        <br /><br />
-        <h5>Already have an account?</h5>
-        <a href="./signin"><button style={buttonStyle}>Sign In</button></a>
-      </div>
       )}
     </div>
   );
