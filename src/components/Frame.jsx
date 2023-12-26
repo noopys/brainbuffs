400d45d273a64f23f840689ec15059fa0f3f39f4
noopys <loganschottland@gmail.com>
2023-12-26 09:12:05 -0700

    Moved video to an S3 bucket

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -72,7 +72,7 @@ const Frame = () => {
                         muted
                         playsInline
                       >
-                        <source src={"/demo_video.mp4"} type="video/mp4" />
+                        <source src={"https://demovideobucketbb.s3.amazonaws.com/demo_video.mp4"} type="video/mp4" />
                         Your browser does not support the video tag.
                       </video>
                     </div>
