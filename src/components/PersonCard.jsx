dffee97a305fc94a41d74c41ba9f661e914d1d9c
mshres24 <mshres24@gmail.com>
2024-01-04 18:14:32 -0700

    Update PersonCard.jsx
    change ui a bit

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -4,7 +4,7 @@ function PersonCard({ name, role, description, imageSrc }) {
   return (
     <div className="Leadership_cardsWrapperLg___T_Y2 lightCyan PersonalCard_strong__ngymj Area-module_baseOneStart__32wGE Area-module_baseTwelveEnd__1U2n3 Area-module_mdOneStart__1-O_g Area-module_mdSixEnd__mW4to">
       <div className="PersonalCard_personalCardGrid__X950m PersonalCard_personalCard__rGKT1 Leadership_card__IstGO">
-        <div className="ContentfulImage_imageContainer__7du_w PersonalCard_cardWrapper__O8GXM" style={{  position: 'relative' }}>
+        <div className="ContentfulImage_imageContainer__7du_w PersonalCard_cardWrapper__O8GXM" style={{ position: 'relative' }}>
           <img
             alt={name}
             src={imageSrc}
@@ -22,8 +22,8 @@ function PersonCard({ name, role, description, imageSrc }) {
         <div className="PersonalCard_cardInfo__atcMh" style={{ marginTop: '10px' }}>
           <div data-testid="bk-taberoni" className="BkTaberoni-module_bkTaberoni__3CE6z lightCyan">
             <div>
-              <h3 className="BkText-module_typeShortSmMd__2Y0v_ BkText-module_bkText__3DMy6 BkText-module_medium__2I-l0 BkTaberoni-module_mainText__3XW3H">{name}</h3>
-              <span className="BkText-module_typeShortSmMd__2Y0v_ BkText-module_bkText__3DMy6 BkText-module_medium__2I-l0 BkType-module_bkType__2QqHN BkTaberoni-module_helperText__32ZRu" style={{ '--base-helper-marginTop': '4px', '--md-helper-marginTop': '4px', '--lg-helper-marginTop': '4px', '--xl-helper-marginTop': '4px' }}>{role}</span>
+              <h3 className="BkText-module_typeShortSmMd__2Y0v_ BkText-module_bkText__3DMy6 BkText-module_medium__2I-l0 BkTaberoni-module_mainText__3XW3H" style={{ fontSize: '40px' }}>{name}</h3>
+              <span className="BkText-module_typeShortSmMd__2Y0v_ BkText-module_bkText__3DMy6 BkText-module_medium__2I-l0 BkType-module_bkType__2QqHN BkTaberoni-module_helperText__32ZRu" style={{ fontWeight: 'bold', '--base-helper-marginTop': '4px', '--md-helper-marginTop': '4px', '--lg-helper-marginTop': '4px', '--xl-helper-marginTop': '4px' }}>{role}</span>
             </div>
           </div>
           <div className="BkText-module_typeShortXs__4YFWW BkText-module_bkText__3DMy6 BkText-module_normal__3U7kp BkType-module_bkType__2QqHN PersonalCard_description__tmK3V">
