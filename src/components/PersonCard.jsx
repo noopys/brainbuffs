8250ed05f29f32f0a25931d07257bf0ade56553e
mshres24 <mshres24@gmail.com>
2024-01-04 18:11:11 -0700

    Update PersonCard.jsx
    update responsivness

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -2,26 +2,33 @@ import React from 'react';
 
 function PersonCard({ name, role, description, imageSrc }) {
   return (
-    <div className="w-full lg:w-[769.1px] xl:w-[736.6px] overflow-hidden shrink-0 flex flex-row items-start justify-start px-0 pb-0 box-border gap-[42px] lg:flex-col lg:items-center">
-      <img
-        className="flex-1 relative max-w-full overflow-hidden h-[294px] object-cover"
-        alt={name}
-        src={imageSrc}
-      />
-      <div className="flex-1 overflow-hidden flex flex-col items-start justify-start gap-[6px]" style= {{textAlign: 'justify'}}>
-        <div className="self-stretch overflow-hidden shrink-0 flex flex-row items-center justify-start gap-[8px]">
-          <div className="relative rounded-lg bg-teal-500 w-[3px] h-[62px]" />
-          <div className="relative font-medium flex items-center shrink-0">
-            <span className="w-full">
-              <p className="m-0">{name}</p>
-              <p className="m-0 text-darkslategray-100">
-                {role}
-              </p>
-            </span>
-          </div>
+    <div className="Leadership_cardsWrapperLg___T_Y2 lightCyan PersonalCard_strong__ngymj Area-module_baseOneStart__32wGE Area-module_baseTwelveEnd__1U2n3 Area-module_mdOneStart__1-O_g Area-module_mdSixEnd__mW4to">
+      <div className="PersonalCard_personalCardGrid__X950m PersonalCard_personalCard__rGKT1 Leadership_card__IstGO">
+        <div className="ContentfulImage_imageContainer__7du_w PersonalCard_cardWrapper__O8GXM" style={{  position: 'relative' }}>
+          <img
+            alt={name}
+            src={imageSrc}
+            decoding="async"
+            className="PersonalCard_cardImage__LHDbT PersonalCard_strong__ngymj"
+            style={{
+              position: 'relative',
+              inset: '0px',
+              width: '300px', // Adjust the size as needed
+              height: '300px', // Adjust the size as needed
+              objectFit: 'cover',
+            }}
+          />
         </div>
-        <div className="self-stretch relative text-lg tracking-[0.18px] leading-[24.3px] text-darkslategray-100">
-          {description}
+        <div className="PersonalCard_cardInfo__atcMh" style={{ marginTop: '10px' }}>
+          <div data-testid="bk-taberoni" className="BkTaberoni-module_bkTaberoni__3CE6z lightCyan">
+            <div>
+              <h3 className="BkText-module_typeShortSmMd__2Y0v_ BkText-module_bkText__3DMy6 BkText-module_medium__2I-l0 BkTaberoni-module_mainText__3XW3H">{name}</h3>
+              <span className="BkText-module_typeShortSmMd__2Y0v_ BkText-module_bkText__3DMy6 BkText-module_medium__2I-l0 BkType-module_bkType__2QqHN BkTaberoni-module_helperText__32ZRu" style={{ '--base-helper-marginTop': '4px', '--md-helper-marginTop': '4px', '--lg-helper-marginTop': '4px', '--xl-helper-marginTop': '4px' }}>{role}</span>
+            </div>
+          </div>
+          <div className="BkText-module_typeShortXs__4YFWW BkText-module_bkText__3DMy6 BkText-module_normal__3U7kp BkType-module_bkType__2QqHN PersonalCard_description__tmK3V">
+            <p>{description}</p>
+          </div>
         </div>
       </div>
     </div>
