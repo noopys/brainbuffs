357309579b6e3a0f0d2cfbcb75c048d9c6d9b59b
noopys <loganschottland@gmail.com>
2024-01-08 13:59:36 -0700

    Loading spinner

--- src/components/homework/Homework.jsx
+++ src/components/homework/Homework.jsx
@@ -146,7 +146,7 @@ function Homework() {
 
   //Don't show homework if user is not logged in 
   if (!isLoggedIn) {
-    return <div style={{ paddingBottom: "100px", paddingTop: "50px", fontSize: "30px", fontFamily: 'poppins'}}>Please sign in to view the adaptive practice system.</div>;
+    return <div style={{ paddingBottom: "100px", paddingTop: "50px", fontSize: "30px", fontFamily: 'poppins' }}>Please sign in to view the adaptive practice system.</div>;
   }
 
   //Homework UI 
@@ -198,39 +198,55 @@ function Homework() {
             )
           }
         </Card>
-        
+
       </div>
       <div className="flex flex-col alignItems-center">
-          <button style={{backgroundColor:"#20a7a1", width: '150px'}} onClick={handleSubmit} className="btn btn-success mt-3 d-block mx-auto px-50">Check</button>
-          <button style={{width: '150px'}} onClick={fetchQuestion} className="btn btn-dark mt-3 d-block mx-auto">Next Question</button>
-          {
-            (isCorrect === "correct") && (
-              <div className="p-3 text-center">
-                <h2 className="text-xl font-bold">Correct!</h2>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                  <path fillRule="evenodd" d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z" clipRule="evenodd" />
-                </svg>
-              </div>
-            )
-          }
-          {
-            (isCorrect === "incorrect") && (
-              <div className="p-3 text-center">
-                <h2 className="text-xl font-bold">Incorrect!</h2>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                  <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
-                </svg>
-
-              </div>
-            )
-          }
+        <div className="flex flex-col items-center md:flex-row md:justify-center">
+          <button
+            style={{ backgroundColor: "#20a7a1", width: '150px' }}
+            onClick={handleSubmit}
+            className="btn btn-success mt-3 px-50"
+          >
+            Check
+          </button>
+          <button
+            style={{ width: '150px' }}
+            onClick={fetchQuestion}
+            className="btn btn-dark mt-3 md:mt-0 md:ml-2"
+          >
+            Next Question
+          </button>
           {isLoading && (
-            <div className="ml-5 mt-2">
+            <div className="mt-3 md:mt-0 md:ml-2">
               <Oval color="#20a7a1" secondaryColor="#20a7a1" />
             </div>
-          )
-          }
+          )}
         </div>
+
+
+
+        {
+          (isCorrect === "correct") && (
+            <div className="p-3 text-center">
+              <h2 className="text-xl font-bold">Correct!</h2>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                <path fillRule="evenodd" d="M19.916 4.626a.75.75 0 01.208 1.04l-9 13.5a.75.75 0 01-1.154.114l-6-6a.75.75 0 011.06-1.06l5.353 5.353 8.493-12.739a.75.75 0 011.04-.208z" clipRule="evenodd" />
+              </svg>
+            </div>
+          )
+        }
+        {
+          (isCorrect === "incorrect") && (
+            <div className="p-3 text-center">
+              <h2 className="text-xl font-bold">Incorrect!</h2>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
+              </svg>
+
+            </div>
+          )
+        }
+      </div>
     </>
 
   );
